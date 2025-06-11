09db2ea7f42afb65aead62d1115672535c76c29f
bekicom <adelkauz6@gmail.com>
2025-06-11 11:33:01 +0500

    api

--- src/context/service/api.service.js
+++ src/context/service/api.service.js
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Create a base query instance for Redux Toolkit Query
 const baseQuery = fetchBaseQuery({
-  // baseUrl: "https://phone-b-navy.vercel.app/api",
-  baseUrl: "http://localhost:8081/api",
+  baseUrl: "https://lolaaftol-b.vercel.app/api",
+  // baseUrl: "http://localhost:8081/api",
   prepareHeaders: (headers, { getState }) => {
     const token = localStorage.getItem("access_token");
 
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -12,8 +12,8 @@ export const Login = memo(() => {
 
     try {
       const res = await axios.post(
-        // "https://phone-b-navy.vercel.app/api/login",
-        "http://localhost:8081/api/login",
+        "https://lolaaftol-b.vercel.app/api/login",
+        // "http://localhost:8081/api/login",
         value
       );
       console.log(res.data);
